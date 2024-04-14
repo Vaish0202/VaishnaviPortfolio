138c307905748f8ad0db5c31bdb738004e4e684a
Vaish0202 <vaishnavilikhe2798@gmail.com>
2024-04-14 20:25:53 +0530

    completed

---
+++ script.js
@@ -0,0 +1,10 @@
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
